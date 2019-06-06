b364d5601f92641d6fad5f889181f6bc30752408
unknown <mikecarleson@example.com>
2019-06-06 16:58:20 -0700

    last push

---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "y",
+  "z"
+];
+
+var guesses = 9;
+var guessesSoFar = [];
+var wins = 0;
+var losses = 0;
+var left = 9;
+var randomLetter;
+
+var newGame = function() {
+	guessesSoFar = [];
+	left = 9;
+	newLetter();
+	guessesLeft();
+	soFar();
+  };
+  
+var guessesLeft = function() {
+	document.getElementById("guesses").innerHTML = "Guesses Left: " + left;
+  };
+
+var soFar = function() {
+  document.getElementById("soFar").innerHTML =
+    "Your Guesses so far: " + guessesSoFar.join(",");
+};
+
+
+var newLetter = function() {
+  randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  alert("A new Letter has been chosen");
+};
+
+
+document.onkeyup = function(event) {
+  var userGuess = event.key;
+  left--;
+  guessesSoFar.push(userGuess);
+  soFar();
+  guessesLeft();
+  if (left > 0) {
+    // Trying to get it to not accept duplicate letters
+    // if(guessesSoFar.includes(userGuess)) {
+    // 	return;
+    //   }
+    if (userGuess == randomLetter) {
+      wins++;
+      document.getElementById("wins").innerHTML = "Wins:" + wins;
+      newGame();
+    }
+  } else if (left == 0) {
+    losses++;
+    document.getElementById("losses").innerHTML = "Losses:" + losses;
+    newGame();
+  }
+};
